0d5a4a02b0624578afb2c4847a54c4ee46fcc311
vitkarpov <viktor.karpov@toptal.com>
2019-07-17 19:17:42 +0300

    Adjusts Timer

--- app/components/timer.js
+++ app/components/timer.js
@@ -3,25 +3,37 @@ const state = require("../lib/state");
 class Timer {
   constructor(props = {}) {
     this.timer = props.timer || 0;
+    let timerId;
 
     state.eventBus.once("mount", () => {
-      this.timerId = setInterval(() => {
+      timerId = setTimeout(() => {
         if (this.timer === 30) {
-          state.lost = true;
-          state.eventBus.emit("rerender");
+          state.set("timeIsOff", true);
         } else {
-          document.querySelector(".timer").innerText = ++this.timer;
+          state.set("timer", this.timer + 1);
         }
+        state.eventBus.emit("rerender");
       }, 1000);
     });
     state.eventBus.once("unmount", () => {
-      state.timer = this.timer;
-      clearInterval(this.timerId);
+      clearTimeout(timerId);
     });
   }
 
+  getColor() {
+    if (this.timer < 15) {
+      return 'green';
+    }
+    if (this.timer < 25) {
+      return 'yellow';
+    }
+    return 'red';
+  }
+
   render() {
-    return `<span class="timer">${this.timer}</span>`;
+    return `
+      You have <span class="timer" style="border: 1px solid ${this.getColor()};">${30 - this.timer}</span> seconds to guess!
+    `;
   }
 }
 
--- app/components/timer.test.js
+++ app/components/timer.test.js
@@ -4,13 +4,4 @@ describe("Timer", () => {
   it("renders correctly", () => {
     expect(new Timer().render()).toMatchSnapshot();
   });
-
-  it("renders correctly after a second pass", () => {
-    const node = document.createElement("div");
-    node.innerHTML = new Timer().render();
-
-    setTimeout(() => {
-      expect(node.querySelector(".timer").innerText).toBe("1");
-    }, 1000);
-  });
 });
